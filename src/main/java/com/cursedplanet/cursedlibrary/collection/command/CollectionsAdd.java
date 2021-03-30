7b6d06bd943aba7519f0201aa628bb2870a554c0
plusgithub <iamarchieferguson@gmail.com>
2021-03-30 18:30:50 +0100

    Changed collections add command to support CMI items

--- src/main/java/com/cursedplanet/cursedlibrary/collection/command/CollectionsAdd.java
+++ src/main/java/com/cursedplanet/cursedlibrary/collection/command/CollectionsAdd.java
@@ -1,18 +1,21 @@
 package com.cursedplanet.cursedlibrary.collection.command;
 
+import com.Zrips.CMI.CMI;
 import com.cursedplanet.cursedlibrary.collection.CollectionLoader;
 import com.cursedplanet.cursedlibrary.collection.CollectionStorage;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
+import com.cursedplanet.cursedlibrary.lib.Common;
 import com.cursedplanet.cursedlibrary.lib.command.SimpleCommandGroup;
 import com.cursedplanet.cursedlibrary.lib.command.SimpleSubCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class CollectionsAdd extends SimpleSubCommand {
 
 	protected CollectionsAdd(SimpleCommandGroup parent) {
 		super(parent, "add|give");
 
-		setUsage("[player]");
+		setUsage("[player] [cmi item id]");
 		setPermission("cursedlibrary.collections.add");
 		setDescription("Adds the item in your hand to selected players collection");
 	}
@@ -28,7 +31,16 @@ public class CollectionsAdd extends SimpleSubCommand {
 			player = Bukkit.getServer().getPlayerExact(args[0]);
 		}
 
-		CollectionStorage.addPlayerItem(player, getPlayer().getInventory().getItemInMainHand());
+		ItemStack item = getPlayer().getInventory().getItemInMainHand();
+		if (args.length >= 2) {
+			try {
+				item = CMI.getInstance().getItemManager().getItem(args[1]).getItemStack();
+			} catch (Exception e) {
+				Common.tell(player, "&cItem could not be found");
+			}
+		}
+
+		CollectionStorage.addPlayerItem(player, item);
 		tell(((String) CollectionLoader.getConfigPart("add_command")).replaceAll("\\{player}", player.getName()));
 	}
 }
